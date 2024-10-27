31f2f670cd612e469214c9b80e6d2d1ccf50c05b
Ikram malik <mr.ikrammaliko1@gmail.com>
2024-10-26 20:50:21 -0700

    updating darkmode after merging

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("hello world");s
+// trying conflicts 
+
+// here we are generating some conflicts in our file so that we can resolve them 
+
+
+// lets see what heppende now we will mix up our text  
+// i think that at this time the dark mode and master branch at same location thats why every thing that i saved in 
+// in master get automatically reflects in dark mode lets see if i push this content it will be shown up in master or i have to 
+// to merge braches again 
